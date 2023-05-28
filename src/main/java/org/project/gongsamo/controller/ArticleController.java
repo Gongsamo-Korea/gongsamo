702a52ed5b7ccca16787e04e6b12abe2ef0c918b
neokkk <seven3126@gmail.com>
2023-05-28 14:58:48 +0900

    feat: 반환 타입 맞추기

--- src/main/java/org/project/gongsamo/controller/ArticleController.java
+++ src/main/java/org/project/gongsamo/controller/ArticleController.java
@@ -2,9 +2,11 @@ package org.project.gongsamo.controller;
 
 import org.project.gongsamo.domain.Article;
 import org.project.gongsamo.dto.article.ArticleDetailDto;
+import org.project.gongsamo.dto.article.ArticleFormDto;
 import org.project.gongsamo.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.*;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,11 +17,18 @@ import java.util.Optional;
 @RequestMapping("/api/articles")
 public class ArticleController {
     private final ArticleService articleService;
+    private final int PAGE_SIZE = 9;
 
     @Autowired
     public ArticleController(ArticleService articleService) {
         this.articleService = articleService;
     }
+
+//    @PostMapping
+//    public ResponseEntity<Article> createArticle(@RequestBody ArticleFormDto article) {
+//        var createdArticle = articleService.createArticle(ArticleFormDto::toEntity(article));
+//        return ResponseEntity.ok(createdArticle);
+//    }
 
     @GetMapping("/{id}")
     public ResponseEntity<Optional<ArticleDetailDto>> article(@PathVariable("id") Long id) {
@@ -30,14 +39,14 @@ public class ArticleController {
     }
 
     @GetMapping
-    public ResponseEntity<List<Article>> allArticles(Pageable pageable) {
-        var articles = articleService.getAllArticles(pageable);
+    public ResponseEntity<List<ArticleDetailDto>> allArticles(@PageableDefault(size = PAGE_SIZE) Pageable pageable) {
+        var articles = articleService.getAllArticles(pageable).stream().map(ArticleDetailDto::from).toList();
         return ResponseEntity.ok(articles);
     }
 
     @GetMapping("/search")
-    public ResponseEntity<List<Article>> searchedArticles(@RequestParam("keyword") String keyword, Pageable pageable) {
-        var articles = articleService.searchArticles(keyword, pageable);
+    public ResponseEntity<List<ArticleDetailDto>> searchedArticles(@RequestParam("keyword") String keyword, @PageableDefault(size = PAGE_SIZE) Pageable pageable) {
+        var articles = articleService.searchArticles(keyword, pageable).stream().map(ArticleDetailDto::from).toList();
         return ResponseEntity.ok(articles);
     }
 
