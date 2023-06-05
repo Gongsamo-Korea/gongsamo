1f4c7b1d5bd3e9bf604fe9953f8b0cdcd55af51d
eunmibean <eunmi.repository@gmail.com>
2023-06-05 23:49:59 +0200

    feat: Apply Swagger 3.0

---
+++ src/main/java/org/project/gongsamo/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package org.project.gongsamo.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Gongsamo API")
+                .description("Gongsamo API")
+                .version("1.0");
+    }
+}
