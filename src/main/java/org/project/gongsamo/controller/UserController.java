d8d7550d71623ecefd4b66f6ac458f84fcd996f3
kimsan <cloudhat17@gmail.com>
2023-06-21 10:04:14 +0900

    deploy test

--- src/main/java/org/project/gongsamo/controller/UserController.java
+++ src/main/java/org/project/gongsamo/controller/UserController.java
@@ -61,7 +61,7 @@ public class UserController {
     @GetMapping("/testPing")
     public ResponseEntity<Object> testPing() {
 
-        return ResponseEntity.ok().body("test-response1");
+        return ResponseEntity.ok().body("test-response");
     }
 
 }
